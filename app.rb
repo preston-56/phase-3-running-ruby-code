7311ea9c0db7303ddcdc5df1a79399866029c3d5
Preston <prestonosoro56@gmail.com>
2023-02-07 21:10:19 +0300

    fix:uncomment lines 6,7 & 8 to pass tests

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@
 #     { id: 3, hello: "Moon" }, 
 #     { id: 4, hello: "Learner" }]
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
+p [1,2,3]
+puts "Hello World!"
+print "Pass this test, please."
 # p [1,2,3]
 
 # def add
